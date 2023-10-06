5386664f2cb57ebcdb18a6e729af58bcc0835df2
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-10-06 16:35:55 +0530

    ss

--- src/Countrys.jsx
+++ src/Countrys.jsx
@@ -1,16 +1,32 @@
-import React, { useEffect } from "react";
-import axios from axios
+import React, { useEffect, useState } from "react";
+import axios from "axios"
+import boostrap
 function State(){
+
+    var [data,setData]=React.useState([])
+     React.useEffect(()=>{
+        axios.get("https://fakestoreapi.com/products").then((res)=>{
+            console.log(res.data)
+            setData([...res.data])
+        })
+    },[])
 
     return (
         <div>
             {
-                useEffect(()=>{
-                    axios.get("https://fakestoreapi.com/products").then((res)=>{
-                        
-                    })
+                data.map((a)=>{
+                return(
+                    <div className="">
+                 <h4>{a.category}</h4>
+                 <h6>{a.price}</h6>
+
+                    </div>
+
+                )
+            
+
                 })
-            }
+           }
         </div>
     )
 
