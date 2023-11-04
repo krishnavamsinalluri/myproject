5db0bac067ed11a81f88e17b532b1a332aaf4ce4
krishnavamsinalluri <vamsinalluri806@gmail.com>
2023-11-04 11:58:49 +0530

    s

--- src/data.jsx
+++ src/data.jsx
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
 import axios from "axios";
-import bootstrap from  "bootstrap"
 function Restdata(){
 
     var [contry,SetContry]=useState([])
     React.useEffect(()=>{
         axios.get("https://restcountries.com/v3/all").then(function(res){
-            console.log(res.data)
-            SetContry(...res.data)
+            console.log(res.data);
+            SetContry(res.data)
         })
-    },[])
-
-    
-
+        
+    },[]);
+    console.log(contry)
+        function ab(){
+            var se=document.getElementById("dd").value;
+            var u=contry.filter((c)=>{
+                return (c.name.common.startsWith(se).toLowerCase())
+            })
+            SetContry(u)
+        }
     return (
-        <div className="box">
-        {
-        contry.map( function(a){
-            return <div className="box">
+        <div className="card w-90">
+            <div className="box">
+                <input type="text" placeholder="search" id="dd" onChange={ab}/>
+
+            </div>
+            <div className="d-flex flex-wrap  junstify-content">
+            {
+         contry.map( function(a){
+            return <div className="m-3 p-3">
                 <h1>{a.name.common}</h1>
+            <img width="200px"  src={a.flags[1]}></img>
             </div>
         })
-         }
+        }
+            </div>
+
+        
+
+
      </div>
     )
-
 }
-export default Restdata
+export default Restdata;
